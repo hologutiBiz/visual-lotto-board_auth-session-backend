5ebd19e3113e9ff8048305eee4029d67a9aa2b66
hologutbiz <holoakinseloyin@gmail.com>
2026-09-26 11:21:36 +0100

    Include lotto forecast app default url in cors origin

--- server.js
+++ server.js
@@ -23,12 +23,13 @@ const SESSION_EXPIRY_MS = 60 * 60 * 24 * 5 * 1000; // 5 days
 // Middleware
 app.use(cors({
     origin: [
-      "https://auth.visuallottoboard.com",
-      "https://pay.visuallottoboard.com",
-      "https://lottoforecast.visuallottoboard.com",
-      "https://app.visuallottoboard.com",
-      "https://premier-lotto-babaijebu-results.visuallottoboard.com",
-      "https://visuallottoboard.com",
+        "https://lotto-forecast-web-db.firebaseapp.com",
+        "https://auth.visuallottoboard.com",
+        "https://pay.visuallottoboard.com",
+        "https://lottoforecast.visuallottoboard.com",
+        "https://app.visuallottoboard.com",
+        "https://premier-lotto-babaijebu-results.visuallottoboard.com",
+        "https://visuallottoboard.com",
 
       // Add more if needed
     ],
